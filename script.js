5d332015424c52384b4e0d3563cc05fd00d0b7c5
Manvith Kumar shetty <121666002+manvithshetty507@users.noreply.github.com>
2024-01-12 15:05:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function nextPalindrome(num) {
   //your JS code here.
+	let cur = ++num;
+	while(true) {
+		if(isPalindrome(cur)) return cur;
+		else cur++;
+	}
+	function isPalindrome(num) {
+		let s = num.toString()
+		let rev = s.split('').reverse().join('')
+		
+		return s === rev
+	}
 }
 
 const input = prompt("Enter a palindrome number");
 alert(nextPalindrome(input));
+
